e0e14e5e9822293ca18171a3c68a7f29d3668173
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2024-01-14 12:15:37 -0800

    Service skeleton.

---
+++ index.ts
@@ -0,0 +1,55 @@
+import {RegisterRoutes} from "./build/routes";
+import express, {
+  Response as ExResponse,
+  Request as ExRequest,
+  NextFunction,
+  json,
+  urlencoded
+} from "express";
+import {ValidateError} from "tsoa";
+import swaggerUi from "swagger-ui-express";
+
+export const app = express();
+
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(json());
+
+RegisterRoutes(app);
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("./build/swagger.json"))
+  );
+});
+
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
